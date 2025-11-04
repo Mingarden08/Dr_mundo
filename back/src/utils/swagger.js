4543bf21e2d655b147cb4c63075b123541cc0a08
Mingarden08 <jeongone.min@gmail.com>
2025-11-04 23:56:11 +0900

    cors!!

--- back/src/utils/swagger.js
+++ back/src/utils/swagger.js
@@ -1,4 +1,5 @@
 const swaggerJSDoc = require("swagger-jsdoc");
+require('dotenv').config();
 
 const options = {
     definition: {
@@ -10,7 +11,7 @@ const options = {
         },
         servers: [
             {
-                url: "http://localhost:3000",
+                url: process.env.DOMAIN,
                 description: "Local server",
             },
         ],

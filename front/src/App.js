64122027eaca407a3e38b9a51aad3d4408afe861
JEONGONE\jeong <jeongone.min@gmail.com>
2025-11-11 03:07:59 +0900

    room error

--- front/src/App.js
+++ front/src/App.js
@@ -4,21 +4,24 @@ import Register from "./pages/Register";
 import Homepage from "./pages/Homepage";
 import RoomPage from "./pages/roompage";
 import WaitingRoom from "./pages/WaitingRoom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { WebSocketProvider } from './WebSocketContext';
 import GamePage from "./pages/GamePage";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/roompage" element={<RoomPage />} />
-        <Route path="/waiting/:roomId" element={<WaitingRoom />} />
-        <Route path="/game/:roomId" element={<GamePage />} />  
-      </Routes>
-    </BrowserRouter>
+    <WebSocketProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/roompage" element={<RoomPage />} />
+          <Route path="/waitingroom/:roomId" element={<WaitingRoom />} />
+          <Route path="/game/:roomId" element={<GamePage />} />
+        </Routes>
+      </Router>
+    </WebSocketProvider>
   );
 }
 
